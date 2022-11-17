03ac5c9a698b5a5bfe0bf33c79ac4dce47e927ad
unknown <deivid.medina@labinfo.is.escuelaing.edu.co>
2022-11-17 13:27:50 -0500

    Subiendo Proyecto

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyecto-Final POOB 2022-02
+
+## Integrantes:
+
+### Gabriel Silva
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+	
+	}
+
+}
