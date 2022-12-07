b1b75d90381a4374086cd26888ccd42cb22b2ac2
gabriel silva <gabriel.silva@mail.escuelaing.edu.co>
2022-12-07 11:09:47 -0500

    Fichas rojas seleccionadas

--- src/main/java/Domain/GestorFicha.java
+++ src/main/java/Domain/GestorFicha.java
@@ -4,13 +4,17 @@ public class GestorFicha {
 
     private FichaNegra inicioN;
 
-    private int fichasN;
+    private FichaRoja inicioR;
+
+    private int fichasN,fichasR;
 
     private Ficha fichaSeleccionada;
 
     public GestorFicha(){
         inicioN = null;
+        inicioR = null;
         agregarNegras();
+        agregarRojas();
     }
 
     public void agregarNegras(){
@@ -28,7 +32,39 @@ public class GestorFicha {
         }
     }
 
-    public void agregarNegra(int x, int y){
+    private void agregarRojas(){
+        int i,j;
+        for (i = 1; i < 4; i++){
+            if (i % 2 == 0) {
+                for (j = 2; j < 9; j = j + 2){
+                    agregarRoja(j,i);
+                }
+            }else {
+                for ( j = 1 ; j < 8; j = j + 2){
+                    agregarRoja(j,i);
+
+                }
+            }
+
+        }
+    }
+
+    public void agregarRoja(int x, int y){
+        FichaRoja ficha = new FichaRoja(x,y);
+
+        if(inicioR == null){
+            inicioR = ficha;
+        }else{
+            inicioR.setPrev(ficha);
+            ficha.setNext(inicioR);
+            inicioR = ficha;
+        }
+
+        fichasR ++;
+
+    }
+
+    private void agregarNegra(int x, int y){
         FichaNegra ficha = new FichaNegra(x,y);
 
         if(inicioN == null){
@@ -43,9 +79,16 @@ public class GestorFicha {
     }
 
     public Ficha[] getFichas(){
-        Ficha[] todas = new Ficha[fichasN];
+        Ficha[] todas = new Ficha[fichasN + fichasR];
+        FichaRoja temp = inicioR;
         FichaNegra temp2 = inicioN;
         int ind = 0;
+
+        while(temp != null){
+            todas[ind++] = temp;
+            temp = temp.getNext();
+
+        }
 
 
         while(temp2!=null){
@@ -75,19 +118,27 @@ public class GestorFicha {
     }
 
     public Ficha buscarFicha(double x, double y){
-
-
-
         int i;
         FichaNegra tempN = inicioN;
+        FichaRoja tempR = inicioR;
 
-        for (i = 0; i < fichasN ; i++){
-            double valorXp =  tempN.getXP();
-            double valorYp =  tempN.getYP();
-            if(tempN.getXP() == x  && tempN.getYP() == y){
-                return tempN;
+        for (i = 0; i < fichasN+fichasR ; i++){
+            //(double valorXp =  tempN.getXP();
+            //double valorYp =  tempN.getYP();
+            if(tempR != null){
+                if (tempR.getXP() == x && tempR.getYP() == y){
+                    return tempR;
+                }
+                tempR = tempR.getNext();
+
+            }else{
+                if(tempN.getXP() == x  && tempN.getYP() == y){
+                    return tempN;
+                }
+                tempN = tempN.getNext();
+
             }
-            tempN = tempN.getNext();
+
         }
         return  null;
     }
@@ -98,6 +149,8 @@ public class GestorFicha {
         Ficha fichaMovida = buscarFicha(x,y);
         if(fichaMovida != null){
             if(fichaMovida.esNegra()){
+                this.seleccionarFicha(x,y);
+            }else if (!fichaMovida.esNegra()){
                 this.seleccionarFicha(x,y);
             }
         }
@@ -116,6 +169,19 @@ public class GestorFicha {
         }
 
         return negras;
+    }
+
+    public  Ficha[] fichasRojas(){
+        Ficha[] rojas = new Ficha[fichasR];
+        FichaRoja temp = inicioR;
+
+        int i = 0;
+        while (temp != null){
+            rojas[i++] = temp;
+            temp = temp.getNext();
+;        }
+
+        return rojas;
     }
 
 }
